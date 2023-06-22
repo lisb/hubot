5127871f1235c485342a1d5333ea27f01a982c51
sho.kuroda <sho.kuroda@l-is-b.com>
2023-06-22 23:37:32 +0900

    BREAKING CHANGE: move types to hubot-direct

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2017 Sho Kuroda <krdlab@gmail.com>
-// Released under the MIT license.
-
-export * from "./direct";
-export * from "./brain";
-export * from "./http";
-export * from "./message";
-export * from "./response";
-export * from "./robot";

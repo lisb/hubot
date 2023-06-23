b9c1c28cf4a4dac2c1a6a3fc220713d4e7bc40d9
sho.kuroda <sho.kuroda@l-is-b.com>
2023-06-23 12:04:08 +0900

    feat(types): export hubot types only

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 L is B Corp.
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import Hubot from "hubot";
+
+export = Hubot;
+export as namespace Hubot;
